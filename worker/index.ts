246f259fa6cb548cd0173ed64e1434b31374ec3a
Alex Mills <alex@acm.io>
2025-06-29 11:52:08 +0800

    Only calling the worker in local mode for headers

--- worker/index.ts
+++ worker/index.ts
@@ -1,19 +1,30 @@
-import { WorkerEntrypoint } from "cloudflare:workers";
+import { WorkerEntrypoint } from "cloudflare:workers"
+
+/*
+
+    Local Development Support
+    -------------------------
+
+    This worker is only used when accessing assets in local
+    wrangler development. In production, the standard cloudflare
+    cache is used with default headers.
+
+*/
 
 export default class extends WorkerEntrypoint<Env> {
   async fetch(request: Request) {
     
     // Fetch Asset
-    const assetResponse = await this.env.ASSETS.fetch(request);
+    const assetResponse = await this.env.ASSETS.fetch(request)
 
-    // Add CORS Headers
+    // Add CORS Headers for local development
     return new Response(assetResponse.body, {
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Methods": "GET",
         "Access-Control-Max-Age": "600",
       }
-    });
+    })
 
   }
 }
