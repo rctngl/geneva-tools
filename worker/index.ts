6b8f9a6fdef404efbd94f35dcd3bbddc0c439ac4
Alex Mills <alex@acm.io>
2025-06-29 09:07:10 +0800

    Added worker function for static assets

---
+++ worker/index.ts
@@ -0,0 +1,19 @@
+import { WorkerEntrypoint } from "cloudflare:workers";
+
+export default class extends WorkerEntrypoint<Env> {
+  async fetch(request: Request) {
+    
+    // Fetch Asset
+    const assetResponse = await this.env.ASSETS.fetch(request);
+
+    // Add CORS Headers
+    return new Response(assetResponse, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+        "Access-Control-Max-Age": "86400",
+      }
+    });
+
+  }
+}
